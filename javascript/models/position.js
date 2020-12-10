f007f10a81726da9d64067e6751e33f9b996d995
almalee24 <flores.almalee@gmail.com>
2020-12-10 15:34:24 +0200

    updated Positions API

--- javascript/models/position.js
+++ javascript/models/position.js
@@ -11,27 +11,36 @@ class Position {
     }
 
     renderPosition(){
-        const positionHolder = document.getElementById("user-position-list")
-        const positionContainer = document.createElement('div')
-        positionContainer.dataset.id = this.id
-        positionContainer.id = this.id
-        positionContainer.classList.add = "position-show"
-        positionContainer.innerHTML += this.positionHTML()
-        positionHolder.appendChild(positionContainer)
-        positionContainer.addEventListener("click", e => {
-            if (e.target.className == "position-button") this.positionUsers(e)
+        const positionContainer = document.getElementById("position-container")
+        const positionCard = document.createElement('div')
+        positionCard.classList.add('position-card')
+        positionCard.id = this.id
+        positionCard.innerHTML += this.positionHTML()
+        positionContainer.appendChild(positionCard)
+        positionCard.addEventListener("click", e => {
+            if (e.target.className.includes("delete")) this.deletePosition(e)
         })
     }
 
     positionHTML(){
-        let available = this.availability == true ? 'yes' : 'no'
         return `
-        <h2 class="headline">${this.title}</h2>
-        <h3 class="position_company">${this.company_name}</h3>
+        <h2 class="position-title">${this.title}</h2>
+        <h3 class="position-company">${this.company_name}</h3>
         <p>Location: ${this.location}</p>
         <p>Description: ${this.description}</p>
         <p>Have you applied? ${this.applied}</p>
         <p>When did you apply? ${this.applied_date}</p>
+        <button class="delete">Delete</button>
         `
+    }
+
+    deletePosition(e){
+        const id = parseInt(e.target.parentElement.id)
+        fetch(`http://localhost:3000/positions/${id}`, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            document.getElmentById('position-container').removeChild(document.getElementById(id))
+        })
     }
 }
--- javascript/services/API.js
+++ javascript/services/API.js
@@ -2,12 +2,40 @@ class API {
 
     static addPositions(){
         fetch("http://localhost:3000/positions")
-        .then(resp => resp.json())
-        .then(positions => {
+            .then(resp => resp.json())
+            .then(positions => {
             positions.forEach(position => {
                 const{id, title, company_name, location, description, applied, applied_date}
                 new Position(id, title, company_name, location, description, applied, applied_date)
             })
+        })
+    }
+
+    static addPosition(e){
+        e.preventDefault()
+
+        let data = {
+            "title": e.target.title.value,
+            "company_name": e.target.company_name.value,
+            "location": e.target.location.value,
+            "description": e.target.description.value,
+            "applied": e.target.applied.value,
+            "applied_date": e.target.applied_date.value
+        };
+        
+        fetch("http://localhost:3000/positions", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        .then(resp => resp.json())
+        .then(position => {
+            const{id, title, company_name, location, description, applied, applied_date}
+            new Position(id, title, company_name, location, description, applied, applied_date)
+            document.getElementById('hog-form').reset()
         })
     }
 
