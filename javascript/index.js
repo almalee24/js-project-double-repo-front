a0ced64d340a39ceb630b35b9570a9ff30035ad2
almalee24 <flores.almalee@gmail.com>
2020-12-14 11:58:02 +0200

    added favicon and logo

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,18 @@
 const buttons = document.querySelectorAll("a");
+const title = document.getElementById("title")
 let el = document.getElementById("content")
 
 buttons.forEach(button => {
     button.addEventListener("click", function(){
         if(this.id == "position_btn"){
+            title.innerHTML = "Position";
             el.innerHTML = '';
             Position_API.addPositions()
         }else if(this.id == "contact_btn"){
+            title.innerHTML = "Contact";
             el.innerHTML = '';
             Contact_API.addContacts()
+
         }else if(this.id == "home_btn"){
             window.location.reload();
         }
@@ -23,3 +27,4 @@ document.addEventListener("DomContentLoaded", function(){
     document.getElementById('position-form').addEventListener("submit", Position_API.addPosition)
     document.getElementById('contact-form').addEventListener("submit", Contact_API.addContact)
 });
+
